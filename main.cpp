e8fdcc6d940ca6a9c53c6cca15dd13817f3cc04c
Catherine Pham <73704202+catherinepham@users.noreply.github.com>
2021-11-15 16:09:37 -0500

    Update main.cpp
    Outline graph class and person object

--- main.cpp
+++ main.cpp
@@ -6,31 +6,36 @@
 #include <map>
 using namespace std;
 
-//make person object here
+struct Person {
+    string name;
+    string company;
+
+    //constructor
+    Person(string _name, string _company){
+        name = _name;
+        company = _company;
+    }
+};
+
 class Graph
 {
 private:
-    map<int, vector<pair<int, int>>> graph;
+    map<Person, vector<Person>> graph;
 
 public:
-    void insertEdge(int from, int to, int weight);
+    void insertEdge(string from, string to);
     void printGraph();
 };
 
-    //insertEdge() adds a new edge between the from and to vertex.
-    void Graph::insertEdge(int from, int to, int weight)
-    {
-        graph[from].push_back(make_pair(to,weight));
-        if(graph.find(to) == graph.end())
-        {
-            graph[to] = {};
-        }
-    }
+//insertEdge() adds a new edge between the from and to vertex.
+void Graph::insertEdge(string from, string to) {
+
+}
 
 void Graph::printGraph()
 {
     //TODO: printGraph() prints the graph in a format sorted by ascending vertex matrix of weights
-    for(auto i: graph)
+    /*for(auto i: graph)
     {
         cout << i.first;
         for(int k = 0; k < i.second.size(); k++)
@@ -38,9 +43,8 @@ void Graph::printGraph()
             cout << " " << i.second[k].first;
         }
         cout << endl;
-    }
+    }*/
 }
-
 
 int main()
 {
